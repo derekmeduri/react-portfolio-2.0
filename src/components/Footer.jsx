a6287899dde3252d01fe22e5e4e6832d9b9ecee2
Derek Meduri <derekmeduri@gmail.com>
2024-07-03 11:16:09 -0400

    adding favicon

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container p-12 flex justify-center">
+                <p className="text-teal-700">Derek Meduri 2024©</p>
+                </div>
+            
+        </footer>
+    )
+}
+
+export default Footer;
