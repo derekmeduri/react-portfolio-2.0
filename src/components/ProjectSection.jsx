71fac74a68024faacd7565475c56b4dd33628d43
Derek Meduri <derekmeduri@gmail.com>
2024-07-10 13:38:42 -0400

    adding derekmeduri.com and projects

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -19,7 +19,7 @@ const projectData = [
         id: 2,
         title: "Vite Portfolio Site",
         description: "Professional portfolio created as final project using vitejs and react components ",
-        image: "/images/projects/1.png",
+        image: "/images/projects/2.png",
         tag: ["All", "Bootcamp"],
         gitUrl: "https://github.com/derekmeduri/react-portfolio",
         previewUrl: "https://derekmeduriportfolio.netlify.app/"
@@ -29,7 +29,7 @@ const projectData = [
         id: 3,
         title: "Profile Energy",
         description: "WIP web application for private client",
-        image: "/images/projects/2.png",
+        image: "/images/projects/3.png",
         tag: ["All", "Solo"],
         gitUrl: "https://github.com/derekmeduri/profileenergy",
         previewUrl: "https://profileenergy.vercel.app/"
@@ -37,60 +37,60 @@ const projectData = [
     },
     {
         id: 4,
-        title: "Project 3 Title",
-        description: "Project 3 description",
+        title: "Weather Forecast App",
+        description: "Application built to show 5 day forecasts. Built with HTML5, CSS, and Javascript",
         image: "/images/projects/3.png",
         tag: ["All", "Bootcamp"],
-        gitUrl: "/",
-        previewUrl: "/"
+        gitUrl: "https://github.com/derekmeduri/weather-forecast",
+        previewUrl: "https://derekmeduri.github.io/weather-forecast/"
     },
     {
         id: 5,
-        title: "Project 4 Title",
-        description: "Project 4 description",
+        title: "Workday Planner App",
+        description: "Application to manage your workday. Built with HTML5, CSS and Javascript",
         image: "/images/projects/4.png",
-        tag: ["All", "Solo"],
-        gitUrl: "/",
-        previewUrl: "/"
+        tag: ["All", "Bootcamp"],
+        gitUrl: "https://github.com/derekmeduri/workday-planner",
+        previewUrl: "https://derekmeduri.github.io/workday-planner/"
 
     },
     {
         id: 6,
-        title: "Project 5 Title",
-        description: "Project 5 description",
-        image: "/images/projects/5.png",
+        title: "Note Taker App",
+        description: "Application to take notes or make todo lists. Built with HTML5, CSS, Javascript, Expressjs",
+        image: "/images/projects/6.png",
         tag: ["All", "Bootcamp"],
-        gitUrl: "/",
-        previewUrl: "/"
+        gitUrl: "https://github.com/derekmeduri/note-taker",
+        previewUrl: "https://meduri-note-taker-86eae7654fa3.herokuapp.com/"
 
     },
     {
         id: 7,
-        title: "Project 6 Title",
-        description: "Project 6 description",
-        image: "/images/projects/6.png",
-        tag: ["All", "Solo"],
-        gitUrl: "/",
-        previewUrl: "/"
+        title: "Password Generator",
+        description: "Simple web application built to randomly generate passwords. Built with HTML5, CSS and Javascript",
+        image: "/images/projects/7.png",
+        tag: ["All", "Bootcamp"],
+        gitUrl: "https://github.com/derekmeduri/password-generator",
+        previewUrl: "https://derekmeduri.github.io/password-generator/"
 
     },
     {
         id: 8,
-        title: "Project 7 Title",
-        description: "Project 7 description",
-        image: "/images/projects/7.png",
+        title: "Nebulas Fables",
+        description: "Interactive learning tool. Built with Handlebars, Javascript and NASA API",
+        image: "/images/projects/8.png",
         tag: ["All", "Bootcamp"],
-        gitUrl: "/",
+        gitUrl: "https://github.com/derekmeduri/nebulas-fables",
         previewUrl: "/"
 
     },
     {
         id: 9,
-        title: "Project 8 Title",
-        description: "Project 8 description",
+        title: "Squabble Sesh",
+        description: "Social Media Application. Built with Vitejs, Javascript, Express, Apollo and MongoDB",
         image: "/images/projects/8.png",
         tag: ["All", "Bootcamp"],
-        gitUrl: "/",
+        gitUrl: "https://github.com/derekmeduri/Squabble-Sesh",
         previewUrl: "/"
 
     }
