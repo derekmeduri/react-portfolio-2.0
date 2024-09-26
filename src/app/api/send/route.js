9019860e07b13008438989aafeb8d37d0959cc24
Derek Meduri <derekmeduri@gmail.com>
2024-09-26 11:42:57 -0400

    adding email

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,30 +1,34 @@
-import { NextResponse } from "next/server";
+"use client";
+import { EmailTemplate } from "@/components/email-template";
+import { data } from "autoprefixer";
 import { Resend } from "resend";
+import React from "react";
+import { configDotenv } from "dotenv";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  "use server";
-
   const { body } = await req.json();
   const { email, subject, message } = body;
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["derekmeduri@gmail.com", email],
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for reaching out!</p>
+          <p>
+            Thank you for reaching out! I will get back to you soon regarding:
+          </p>
           <p>{message}</p>
         </>
       ),
     });
 
-    return NextResponse.json(data);
+    return Response.json({ data });
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return Response.json({ error }, { status: 500 });
   }
 }
